d27551e2c51059e4dc8579a198de71a5cf3726ef
xiaoxuetu <3453899898@qq.com>
2026-02-02 17:02:02 +0800

    Add two commonly used mathematical functions, the conversion between degrees and radians.

--- libs/Math/include/Math/CSMathUtility.h
+++ libs/Math/include/Math/CSMathUtility.h
@@ -6,13 +6,15 @@
 #define MATH_CSMATHUTILITY_H
 
 /* 有用的一些常数 */
-
 #define CS_PI (3.1415926535897932f)
 
 
 #define CS_KINDA_SMALL_NUMBER (1.e-4f)
 
-
 #define CS_DELTA (0.00001f)
+
+/* 角度和弧度互转 */
+#define CS_AngleToRadian(angle) (angle * CS_PI / 180.0f)
+#define CS_AngleToRadian(Radian) (Radian * 180.0f / CS_PI)
 
 #endif //MATH_CSMATHUTILITY_H
